45ac92299f3348d4a23f8d1309c5b38cbfc45b4b
subapriya2111 <subapriya2111@gmail.com>
2026-07-08 12:38:37 +0530

    Add files via upload

---
+++ day2hw.js
@@ -0,0 +1,20 @@
+let userName="playwright"
+console.log(userName);
+console.log(typeof userName);
+
+let organization=null
+console.log(organization);
+console.log(typeof organization);
+
+
+let contactNumber=54865676
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation=true
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
