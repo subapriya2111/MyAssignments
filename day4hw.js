f84741994841fc6525fdb6e54ed765ba33fac04b
subapriya2111 <subapriya2111@gmail.com>
2026-07-13 12:36:59 +0530

    Add files via upload

---
+++ day4hw.js
@@ -0,0 +1,9 @@
+//squares in reverse order using for loop in javascript
+
+function squaresInReverse() {
+    for (let i = 10; i >= 1; i--) {
+   let x = i*i;
+    console.log("The square root of", i , "is" , x);      
+    }
+}
+squaresInReverse();
